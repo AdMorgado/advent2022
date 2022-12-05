7c4b7f5c221468b731dcba4c5a2cf0834f7de88b
AdMorgado <73718959+AdMorgado@users.noreply.github.com>
2022-12-05 13:14:13 +0000

    fifth day

---
+++ 05/part2.py
@@ -0,0 +1,39 @@
+
+filename = "input.txt"
+
+stacks = []
+moves = []
+
+def getElements(line):
+    return [line[i] for i in range(1, len(line)-1, 4)]
+
+def makeIntoMove(line):
+    proto = line.split(" ")
+    return [int(proto[1]), int(proto[3]), int(proto[5])]
+
+with open(filename, "r") as file:
+    stackInput, moveInput = file.read().split("\n\n")
+    elems = list(map(getElements, stackInput.split("\n")[:-1]))
+    stackWidth = len(elems[0])
+    stacks = [[] for i in range(stackWidth)]
+    for y in range(len(elems)-1, -1, -1):
+        for x in range(stackWidth):
+            elem = elems[y][x]
+            if elem != " ": stacks[x].append(elem)
+
+    moves = list(map(makeIntoMove, moveInput.splitlines()))
+
+def makeMove(stacks, move):
+    sorc = move[1]-1
+    dest = move[2]-1
+
+    removed = stacks[sorc][-move[0]:]
+    stacks[sorc] = stacks[sorc][:-move[0]]
+    
+    stacks[dest] += removed
+
+for move in moves:
+    makeMove(stacks, move)
+
+result = [stack.pop() for stack in stacks]
+print("".join(result))
